168b4ebf70cdc80de22564fe4212b97f169dbfd5
Zadnoor <zadnoor@fantacode.com>
2023-12-06 00:16:30 +0530

    Add unit tests for the Python code used in the Lambda function using pytest

--- README.md
+++ README.md
@@ -9,13 +9,7 @@ This CDK Project achieves:
 
 The Nginx Ingress controller replica count is configured by an SSM Parameter value. When the SSM Parameter value is 
 `staging` or `production`, then the replicacount is set to 2. If the parameter is `development` or something undefined,
-the replicacount will be set to 1. The Helm replica count is updated using the Lambda CustomResource.
-
-As of now, the SSM Parameter creation is not part of this CDK Project, and it assumes the SSM Parameter already exists. 
-This has not been added to this project yet because the SSM parameter creation, when it's a fresh install, happens at 
-deploy time, and the SSM parameter fetch with Boto3 happens even before the synth happens, so once we can define 
-the ssm parameter creation within Cloudformation as a dependency of boto3 ssm fetch, we can add it to this CDK Project. 
-
+the replicacount will be set to 1. The Helm replica values map is returned by the CustomResource Lambda function.
 
 # Setting up CDK
 
@@ -32,4 +26,14 @@ cdk synth
 
 # To deploy the cdk project
 cdk deploy
+```
+
+
+# Running Unit Tests with pytest
+
+Make sure the pytest package is installed, and then run `pytest`
+
+```bash
+pip install -r requirements.txt
+pytest
 ```
--- tests/unit/test_cdk_python_stack.py
+++ tests/unit/test_cdk_python_stack.py
@@ -1,15 +1,64 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+import pytest
+import os
+import sys
+import boto3
+import json
 
-from cdk_python.cdk_python_stack import CdkPythonStack
+resource_directory = os.path.abspath('resources/LambdaCustomResource')
+sys.path.append(resource_directory)
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_python/cdk_python_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkPythonStack(app, "cdk-python")
-    template = assertions.Template.from_stack(stack)
+from index import on_event
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+SSM_PARAM_NAME = "/platform/account/env_test"
+
+def obtain_helm_with_environment(env_name, expected_replica_count):
+    # First put the "development" value in SSM
+    ssm = boto3.client('ssm')
+    ssm.put_parameter(
+        Name=SSM_PARAM_NAME,
+        Type='String',
+        Value=env_name,
+        Overwrite=True
+    )
+
+    event_data = {
+        "RequestType": "Update",
+        "PhysicalResourceId": "InputResourceId",
+        "ResourceProperties": {
+            "ssm_parameter_name": SSM_PARAM_NAME
+        }
+    }
+
+    helm_value = json.dumps({
+        "controller": {
+            "replicaCount": expected_replica_count
+        }
+    })
+    expected_response = {
+        "PhysicalResourceId": "InputResourceId",
+        "Data": {
+            "helm_values": helm_value
+        }
+    }
+    return event_data, expected_response
+
+def test_lambda_development():
+    environment_name = 'development'
+    expected_replica_count = 1
+    event_data, expected_response = obtain_helm_with_environment(environment_name, expected_replica_count)
+
+    assert on_event(event_data, 'context') == expected_response
+
+def test_lambda_staging():
+    environment_name = 'staging'
+    expected_replica_count = 2
+    event_data, expected_response = obtain_helm_with_environment(environment_name, expected_replica_count)
+
+    assert on_event(event_data, 'context') == expected_response
+
+def test_lambda_production():
+    environment_name = 'production'
+    expected_replica_count = 2
+    event_data, expected_response = obtain_helm_with_environment(environment_name, expected_replica_count)
+
+    assert on_event(event_data, 'context') == expected_response
